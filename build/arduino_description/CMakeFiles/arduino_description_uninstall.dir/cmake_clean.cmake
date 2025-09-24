618aa1acfc60044bc5f261014e08caf7b6340e00
JuliusKanneh <jkanneh@andrew.cmu.edu>
2025-09-25 01:42:18 +0200

    added the arduino_description pkg for modeling the robert for visualization.

---
+++ build/arduino_description/CMakeFiles/arduino_description_uninstall.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/arduino_description_uninstall"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/arduino_description_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
